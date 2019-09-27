b39c6aaae7306ef0f9fbb3469d4cb61c7dd3a958
Stein Somers <git@steinsomers.be>
2019-09-27 11:21:51 +0200

    disjoint sets weren't tested

--- src/tests/set.rs
+++ src/tests/set.rs
@@ -66,26 +66,26 @@ proptest! {
     })]
 
     #[test]
-    fn test_difference_future(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
+    fn difference_future_arbitrary(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
         assert_difference(difference_future(&s1, &s2), &s1, &s2)?
     }
 
     #[test]
-    fn test_difference_future_aligned_left(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+    fn difference_future_aligned_left(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::min_value());
         s2.insert(u8::min_value());
         assert_difference(difference_future(&s1, &s2), &s1, &s2)?
     }
 
     #[test]
-    fn test_difference_future_aligned_right(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+    fn difference_future_aligned_right(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::max_value());
         s2.insert(u8::max_value());
         assert_difference(difference_future(&s1, &s2), &s1, &s2)?
     }
 
     #[test]
-    fn test_difference_future_aligned_both(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+    fn difference_future_aligned_both(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::min_value());
         s2.insert(u8::min_value());
         s1.insert(u8::max_value());
@@ -94,13 +94,13 @@ proptest! {
     }
 
     #[test]
-    fn test_is_subset_future(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
+    fn is_subset_future_arbitrary(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
         prop_assert_eq!(difference_future(&s1, &s2).next().is_none(),
                         is_subset_future(&s1, &s2))
     }
 
     #[test]
-    fn test_is_subset_future_aligned_left(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+    fn is_subset_future_aligned_left(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::min_value());
         s2.insert(u8::min_value());
         prop_assert_eq!(difference_future(&s1, &s2).next().is_none(),
@@ -108,17 +108,7 @@ proptest! {
     }
 
     #[test]
-    fn test_is_subset_future_aligned_right(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
-        s1.insert(u8::max_value());
-        s2.insert(u8::max_value());
-        prop_assert_eq!(difference_future(&s1, &s2).next().is_none(),
-                        is_subset_future(&s1, &s2))
-    }
-
-    #[test]
-    fn test_is_subset_future_aligned_both(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
-        s1.insert(u8::min_value());
-        s2.insert(u8::min_value());
+    fn is_subset_future_aligned_right(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::max_value());
         s2.insert(u8::max_value());
         prop_assert_eq!(difference_future(&s1, &s2).next().is_none(),
@@ -126,21 +116,31 @@ proptest! {
     }
 
     #[test]
-    fn test_intersection_future_aligned_left(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+    fn is_subset_future_aligned_both(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+        s1.insert(u8::min_value());
+        s2.insert(u8::min_value());
+        s1.insert(u8::max_value());
+        s2.insert(u8::max_value());
+        prop_assert_eq!(difference_future(&s1, &s2).next().is_none(),
+                        is_subset_future(&s1, &s2))
+    }
+
+    #[test]
+    fn intersection_future_aligned_left(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::min_value());
         s2.insert(u8::min_value());
         assert_intersection(intersection_future(&s1, &s2), &s1, &s2)?
     }
 
     #[test]
-    fn test_intersection_future_aligned_right(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+    fn intersection_future_aligned_right(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::max_value());
         s2.insert(u8::max_value());
         assert_intersection(intersection_future(&s1, &s2), &s1, &s2)?
     }
 
     #[test]
-    fn test_intersection_future_aligned_both(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
+    fn intersection_future_aligned_both(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>) {
         s1.insert(u8::min_value());
         s2.insert(u8::min_value());
         s1.insert(u8::max_value());
@@ -149,12 +149,31 @@ proptest! {
     }
 
     #[test]
-    fn test_intersection_future(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
+    fn intersection_future_disjoint(mut s1: BTreeSet<u8>, mut s2: BTreeSet<u8>, split: u8) {
+        while let Some(&max) = s1.iter().next_back() {
+            if max > split {
+                s1.remove(&max);
+            } else {
+                break
+            }
+        }
+        while let Some(&min) = s2.iter().next() {
+            if min < split {
+                s2.remove(&min);
+            } else {
+                break
+            }
+        }
         assert_intersection(intersection_future(&s1, &s2), &s1, &s2)?
     }
 
     #[test]
-    fn test_intersection_swivel(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
+    fn intersection_future_arbitrary(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
+        assert_intersection(intersection_future(&s1, &s2), &s1, &s2)?
+    }
+
+    #[test]
+    fn intersection_swivel_arbitrary(s1: BTreeSet<u8>, s2: BTreeSet<u8>) {
         assert_intersection(intersection_swivel(&s1, &s2), &s1, &s2)?;
     }
 }
